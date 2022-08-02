5e833b64ae4b3b751cc7ca0ad4379305263f49ed
MEMCIA <84227329+MEMCIA@users.noreply.github.com>
2022-08-02 22:17:13 +0200

    error fixed

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.addEventListener('load', () => {
 	const form = document.querySelector("#new-task-form");
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
-    console.log(form);
 
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
+        console.log("Submit form");
 
 		const task = input.value;
 
